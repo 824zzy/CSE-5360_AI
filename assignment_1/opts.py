8dcd4beacd8880e71a5952617c9730776b0d8847
824zzy <614594368@qq.com>
2019-09-07 22:04:26 -0500

    fix($location): default config

--- assignment_1/opts.py
+++ assignment_1/opts.py
@@ -5,11 +5,10 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     
     parser.add_argument('--data_dir', type=str, default='./data/')
-    parser.add_argument('--input_filename', type=str, default='input1.txt')
-    parser.add_argument('--origin_city', type=str, default='Bremen')
-    parser.add_argument('--destination_city', type=str, default='Kassel')
-    # default heuristic suppose to be h_kassel.txt.
-    parser.add_argument('--heuristic_filename', type=str, default='')
+    parser.add_argument('input_filename', nargs='?', type=str, default='input1.txt')
+    parser.add_argument('origin_city', nargs='?', type=str, default='Bremen')
+    parser.add_argument('destination_city', nargs='?', type=str, default='Kassel')
+    parser.add_argument('heuristic_filename', nargs='?', type=str, default='')
     
     args = parser.parse_args()
     return args
